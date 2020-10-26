90938baaaa215eeb44e4de94b918083c9fb88c16
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-10-26 17:52:00 +0100

    Stampati contenuti db nella view

--- project/resources/views/students.blade.php
+++ project/resources/views/students.blade.php
@@ -2,11 +2,34 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
     <title>Lista Studenti</title>
   </head>
   <body>
 
-    
+    <div class="wrapper">
+      <div class="title">
+        <h1>Elenco Studenti</h1>
+      </div>
+
+      <table>
+        <tr>
+          <th>Nome</th>
+          <th>Cognome</th>
+          <th>Età</th>
+          <th>Sesso</th>
+        </tr>
+        @foreach($students as $student)
+          <tr>
+            <td>{{$student->name}}</td>
+            <td>{{$student->lastname}}</td>
+            <td>{{$student->age}}</td>
+            <td>{{$student->gender}}</td>
+          </tr>
+        @endforeach
+      </table>
+      
+    </div>
 
   </body>
 </html>
